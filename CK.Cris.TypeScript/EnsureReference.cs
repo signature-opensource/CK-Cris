cf84f0cef545724cc10bd39ebedf58b4b7e3c934
Olivier Spinelli <olivier.spinelli@signature-code.com>
2021-08-02 12:29:49 +0200

    Debugging CKSetup analysis.

---
+++ CK.Cris.TypeScript/EnsureReference.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CK.Cris.TypeScript
+{
+    static class EnsureReference
+    {
+        class Fake : CK.StObj.TypeScript.TypeScriptAttribute
+        {
+        }
+    }
+}
