19cb53c2082c9426e7d9bc457529689077246ca7
Kuinox <github@kuinox.io>
2022-10-24 17:25:21 +0200

    Added ICrisEndpoint default implementation: HttpCrisEndpoint.

--- CK.Cris.Engine/TypeScriptSupport/TypeScriptCrisCommandGeneratorImpl.cs
+++ CK.Cris.Engine/TypeScriptSupport/TypeScriptCrisCommandGeneratorImpl.cs
@@ -199,7 +199,10 @@ namespace CK.Setup
 
         static void InitializeCrisModelFile( IActivityMonitor monitor, TypeScriptFile<TypeScriptContextRoot> fModel )
         {
-            fModel.EnsureImport( monitor, typeof( VESACode ), typeof( ICrisResultError ) );
+            fModel.EnsureImport( monitor, typeof( VESACode ), typeof( ICrisResultError ), typeof(ICrisResult) );
+            fModel.Imports.EnsureImportFromLibrary( new LibraryImport( "axios", "^0.27.2", DependencyKind.Dependency ),
+                "Axios", "AxiosInstance"
+                );
             fModel.Body.Append( @"
 
 export type ICommandResult<T> = {
@@ -229,6 +232,54 @@ export interface Command<TResult = void> {
 
 export interface ICrisEndpoint {
  send<T>(command: Command<T>): Promise<ICommandResult<T>>;
+}
+
+export class HttpCrisEndpoint implements ICrisEndpoint {
+  constructor(private readonly axios: Axios) {
+  }
+
+  async send<T>(command: Command<T>): Promise<ICommandResult<T>> {
+    try {
+      let string = `[""${command.commandModel.commandName}""`;
+      string += `,${JSON.stringify(command, (key, value) => {
+        return key == ""commandModel"" ? undefined : value;
+      })}]`;
+      const resp = await this.axios.post<string>('', string);
+      
+      const result = JSON.parse(resp.data)[1] as CrisResult; // TODO: @Dan implement io-ts.
+      if (result.code == VESACode.Synchronous) {
+        return {
+          code: VESACode.Synchronous,
+          result: result.result as T,
+          correlationId: result.correlationId
+        };
+      }
+      else if (result.code == VESACode.Error || result.code == VESACode.ValidationError) {
+        return {
+          code: result.code as VESACode.Error | VESACode.ValidationError,
+          result: result.result as CrisResultError,
+          correlationId: result.correlationId
+        };
+      }
+      else if (result.code == VESACode.Asynchronous) {
+        throw new Error(""Endpoint returned VESACode.Asynchronous which is not yet supported by this client."");
+      } else {
+        throw new Error(""Endpoint returned an unknown VESA Code."");
+      }
+    } catch (e) {
+      if (e instanceof Error) {
+        return {
+          code: 'CommunicationError',
+          result: e
+        };
+      } else {
+        return {
+          code: 'CommunicationError',
+          result: new Error(`Unknown error ${e}.`)
+        };
+      }
+    }
+  }
 }
 " );
         }
