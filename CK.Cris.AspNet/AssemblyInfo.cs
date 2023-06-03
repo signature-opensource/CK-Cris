ca8267ce234bcc701a62811f0b9e0a61547caad9
Olivier Spinelli <olivier.spinelli@signature-code.com>
2023-06-04 00:51:02 +0200

    Trimming occurred.
    CK.Cris.AspNet reference to CK.Cris.TypeScript has been
    "optimized". Added a reference.
    CKSetup error message:
    
    E [] Cannot register 'CK.Cris.AspNet (CK.Cris.AspNet.dll)',
    'CK.Cris.TypeScript (CK.Cris.TypeScript.dll)' at the same time.
    They must be registered individually.
    
    This message should be a little bit more verbose!

--- CK.Cris.AspNet/AssemblyInfo.cs
+++ CK.Cris.AspNet/AssemblyInfo.cs
@@ -1,3 +1,14 @@
+using System;
+
 [assembly: CK.Setup.IsModel()]
 [assembly: CK.Setup.RequiredSetupDependency( "CK.Cris.AspNet.Engine" )]
 
+[assembly: EnsureReference( typeof(CK.Cris.TypeScriptCrisCommandGenerator ))]
+
+[AttributeUsage( AttributeTargets.Assembly )]
+class EnsureReferenceAttribute : System.Attribute
+{
+    public Type Target { get; }
+
+    public EnsureReferenceAttribute( Type target ) => Target = target;
+}
