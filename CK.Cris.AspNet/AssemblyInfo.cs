1fed521b897d61f35e2e67dd16d956ca684ada77
Olivier Spinelli <olivier.spinelli@signature-code.com>
2023-06-04 12:10:37 +0200

    Prepares the future CK.Core.
    Must test the CrisEventHub.

--- CK.Cris.AspNet/AssemblyInfo.cs
+++ CK.Cris.AspNet/AssemblyInfo.cs
@@ -3,12 +3,4 @@ using System;
 [assembly: CK.Setup.IsModel()]
 [assembly: CK.Setup.RequiredSetupDependency( "CK.Cris.AspNet.Engine" )]
 
-[assembly: EnsureReference( typeof(CK.Cris.TypeScriptCrisCommandGenerator ))]
-
-[AttributeUsage( AttributeTargets.Assembly )]
-class EnsureReferenceAttribute : System.Attribute
-{
-    public Type Target { get; }
-
-    public EnsureReferenceAttribute( Type target ) => Target = target;
-}
+[assembly: PreserveAssemblyReference( typeof( CK.Cris.TypeScriptCrisCommandGenerator ))]
--- CK.Cris.Executor/CrisEventHub/DarkSideCrisEventHub.cs
+++ CK.Cris.Executor/CrisEventHub/DarkSideCrisEventHub.cs
@@ -12,9 +12,18 @@ namespace CK.Cris
         {
             _hub = hub;
         }
+        WIP
 
+        /// <summary>
+        /// Gets the immediate event sender.
+        /// This sender automatically relays its events to the <see cref="AllEventSender"/> (see
+        /// <see cref="PerfectEventSender{TEvent}.CreateRelay(PerfectEventSender{TEvent}, bool)"/>).
+        /// </summary>
         public PerfectEventSender<IEvent> ImmediateSender => _hub._immediate;
 
+        /// <summary>
+        /// Gets the sender of all events.
+        /// </summary>
         public PerfectEventSender<IEvent> AllEventSender => _hub._allEvent;
     }
 
