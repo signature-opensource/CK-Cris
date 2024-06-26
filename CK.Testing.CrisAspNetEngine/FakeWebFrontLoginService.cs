b50a475d779a7fe1e762091d3bf62d06de744ebb
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-06-26 10:20:54 +0200

    Opened and documented FakeUserDatabase & FakeWebFrontLoginService.

--- CK.Testing.CrisAspNetEngine/FakeWebFrontLoginService.cs
+++ CK.Testing.CrisAspNetEngine/FakeWebFrontLoginService.cs
@@ -12,8 +12,15 @@ using System.Threading.Tasks;
 
 namespace CK.Testing
 {
-
-    public sealed class FakeWebFrontLoginService : IWebFrontAuthLoginService
+    /// <summary>
+    /// Fake login service bound to the <see cref="FakeUserDatabase"/> that
+    /// implements only "Basic" login based on the <see cref="FakeUserDatabase.AllUsers"/> availability
+    /// and password "success" for every existing users.
+    /// <para>
+    /// This class is totally opened to specialization.
+    /// </para>
+    /// </summary>
+    public class FakeWebFrontLoginService : IWebFrontAuthLoginService
     {
         readonly IAuthenticationTypeSystem _typeSystem;
         readonly FakeUserDatabase _userDB;
@@ -24,20 +31,16 @@ namespace CK.Testing
             _userDB = userDB;
         }
 
-        // Waiting for .Net...10? (https://github.com/dotnet/runtime/issues/31001):
-        // IList<T> should be IReadOnlyList<T>. 
-        public IReadOnlyList<IUserInfo> AllUsers => _userDB.AllUsers.ToArray();
+        public virtual bool HasBasicLogin => true;
 
-        public bool HasBasicLogin => true;
+        public virtual IReadOnlyList<string> Providers => new string[] { "Basic" };
 
-        public IReadOnlyList<string> Providers => new string[] { "Basic" };
-
-        public object CreatePayload( HttpContext ctx, IActivityMonitor monitor, string scheme )
+        public virtual object CreatePayload( HttpContext ctx, IActivityMonitor monitor, string scheme )
         {
             throw new NotSupportedException();
         }
 
-        public Task<UserLoginResult> BasicLoginAsync( HttpContext ctx, IActivityMonitor monitor, string userName, string password, bool actualLogin )
+        public virtual Task<UserLoginResult> BasicLoginAsync( HttpContext ctx, IActivityMonitor monitor, string userName, string password, bool actualLogin )
         {
             IUserInfo? u = null;
             if( password == "success" )
@@ -54,7 +57,7 @@ namespace CK.Testing
             return Task.FromResult( new UserLoginResult( null, 1, "Login failed!", false ) );
         }
 
-        public Task<UserLoginResult> LoginAsync( HttpContext ctx, IActivityMonitor monitor, string providerName, object payload, bool actualLogin )
+        public virtual Task<UserLoginResult> LoginAsync( HttpContext ctx, IActivityMonitor monitor, string providerName, object payload, bool actualLogin )
         {
             if( providerName != "Basic" ) throw new ArgumentException( "Unknown provider.", nameof( providerName ) );
             var o = payload as List<KeyValuePair<string, object>>;
@@ -62,9 +65,16 @@ namespace CK.Testing
             return BasicLoginAsync( ctx, monitor, (string)o.FirstOrDefault( kv => kv.Key == "userName" ).Value, (string)o.FirstOrDefault( kv => kv.Key == "password" ).Value, actualLogin );
         }
 
-        public Task<IAuthenticationInfo> RefreshAuthenticationInfoAsync( HttpContext ctx, IActivityMonitor monitor, IAuthenticationInfo current, DateTime newExpires )
+        public virtual Task<IAuthenticationInfo> RefreshAuthenticationInfoAsync( HttpContext ctx, IActivityMonitor monitor, IAuthenticationInfo current, DateTime newExpires )
         {
-            throw new NotSupportedException( "Not tested." );
+            var stillHere = _userDB.AllUsers.FirstOrDefault( i => i.UserName == current.UnsafeUser.UserName );
+            if( stillHere != null )
+            {
+                monitor.Info( $"Refreshed authentication for '{current.UnsafeUser.UserName}'." );
+                return Task.FromResult( current.SetExpires( newExpires ) );
+            }
+            monitor.Info( $"Failed to refres authentication for '{current.UnsafeUser.UserName}'." );
+            return Task.FromResult( _typeSystem.AuthenticationInfo.None );
         }
     }
 
