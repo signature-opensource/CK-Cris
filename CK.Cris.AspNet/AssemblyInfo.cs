0b8810a8eb1f453929f34aba93f2ead9290b42b4
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-07-01 15:42:18 +0200

    Fix assembly info definition.

--- CK.Cris.AspNet/AssemblyInfo.cs
+++ CK.Cris.AspNet/AssemblyInfo.cs
@@ -1,5 +1,8 @@
 using System;
 
-[assembly: CK.Setup.IsModelDependent()]
+[assembly: CK.Setup.IsPFeature()]
+//[assembly: CK.Setup.RequiredEngine( "CK.Cris.Engine" )]
+
+[assembly: CK.Setup.IsModel()]
 [assembly: CK.Setup.RequiredSetupDependency( "CK.Cris.AspNet.Engine" )]
 
