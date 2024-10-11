2ea92afb4386a1388454281d71a039fb21f1eaa6
Olivier Spinelli <olivier.spinelli@signature-code.com>
2024-10-11 16:43:16 +0200

    Fixed comment.

--- CK.Cris/Attributes/IAllowUnclosedCommandAttribute.cs
+++ CK.Cris/Attributes/IAllowUnclosedCommandAttribute.cs
@@ -6,8 +6,8 @@ namespace CK.Cris;
 /// Attribute marker for parameter attribute that states that a command
 /// handler allows its command to not be the "unified", "closed", interface.
 /// <para>
-/// This interface is like <see cref="IAutoService"/> and its friends: only its name matter
-/// and it can be locally defined in any assemblies that need it.
+/// This interface is duck typed: only its name matter and it can be locally defined
+/// in any assemblies that need it.
 /// </para>
 /// </summary>
 public interface IAllowUnclosedCommandAttribute
